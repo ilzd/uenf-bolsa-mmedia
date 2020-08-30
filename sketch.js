6546a22f4023a49ca0507ff9ff1c98397954bb83
lzd <ianlouzad4@gmail.com>
2020-08-30 20:52:41 -0300

    full screen canvas / fixing scroll bars

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
 function setup(){
-    createCanvas(600, 600);
+    createCanvas(windowWidth, windowHeight); //Setting canvas size to cover the entire window
     background(255, 0, 0);
+
+    
+    let canvas = document.getElementById('defaultCanvas0'); //Capturing p5js canvas from the document
+    canvas.style.display = 'block'; //To avoid displaying scroll bars
 }
 
 function draw(){
